8ddae6a8edcf0997b5afd258fd520fe2a0ebe2b9
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-07-08 21:12:08 +0200

    Refactoring

--- botCommands/cleanup.js
+++ botCommands/cleanup.js
@@ -6,24 +6,20 @@ axios.default.defaults.headers.patch.Authorization = `Token ${config.pointsbot.t
 
 const cleanUpPointsDatabase = {
   regex: /^!odin please cleanup!$/g,
-  cb: ({ author, guild }) => {
+  cb: async ({ author, guild }) => {
     const user = guild.members.cache.get(author.id);
     const isAdmin = user.roles.cache.some((value) => config.roles.adminRolesName.includes(value.name))
     if (isAdmin) {
-      const active_discord_ids = guild.members.cache.map((member) => member.id);
-      sendCleaningRequest(active_discord_ids);
+      try {
+        const active_discord_ids = guild.members.cache.map((member) => member.id);
+        await axios.patch('http://localhost:3000/api/points/update', {
+          active_discord_ids
+        })
+        return "Cleaned up successfully!!!"
+      } catch (err) {
+        return "Couldn't clean up, error occured"
+      }
     }
-
-  }
-}
-
-async function sendCleaningRequest(active_discord_ids) {
-  try {
-    await axios.patch('http://localhost:3000/api/points/update', {
-      active_discord_ids
-    })
-  } catch (err) {
-    console.log(err);
   }
 }
 

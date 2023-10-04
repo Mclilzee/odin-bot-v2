0ac975e1a80027d20dfbac078a475478cb47ad5a
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2023-10-04 19:12:51 +0200

    Add chat bombing auto bot response

---
+++ botCommands/chatbomb.js
@@ -0,0 +1,33 @@
+const { registerBotCommand } = require('../botEngine');
+let messagesCount = 0;
+let authorID = undefined;
+let timeoutID = undefined;
+
+const command = {
+  cb: ({ author }) => {
+    if (authorID === author.id) {
+      messagesCount++;
+      if (timeoutID === undefined) {
+        timeoutID = setTimeout(() => messagesCount = 0, 10000);
+      }
+    } else {
+      if (timeoutID !== undefined) {
+        clearTimeout(timeoutID);
+      }
+      authorID = author.id;
+      messagesCount = 1;
+    }
+
+    if (messagesCount >= 5) {
+      return "Stop it!! that's illegal you can't chat bomb";
+    }
+
+    console.log("timeout", timeoutID);
+    console.log("lastauthor", author.id);
+    console.log("messageCount", messagesCount);
+  }
+};
+
+registerBotCommand(command.regex, command.cb);
+
+module.exports = command;
